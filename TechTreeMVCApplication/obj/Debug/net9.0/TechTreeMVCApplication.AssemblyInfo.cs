19c0c85397c06fc5757552fdeab111375c462fa6
Sebastian <spinzonrojas@gmail.com>
2025-07-17 23:31:14 -0500

    Interactuando con las vistas

--- TechTreeMVCApplication/obj/Debug/net9.0/TechTreeMVCApplication.AssemblyInfo.cs
+++ TechTreeMVCApplication/obj/Debug/net9.0/TechTreeMVCApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechTreeMVCApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914480d20c8841ec329f2754e51329b7a560cfc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2018fbec0b36fc075200411079f1e77075cb24")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechTreeMVCApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechTreeMVCApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
